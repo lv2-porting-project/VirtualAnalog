0782723d30ccfe6d9c773fe4577e5c13c7f4f46c
KottV <kill_it@mail.ru>
2020-05-21 15:13:27 +1000

    add LV2 compile stuff

---
+++ plugin/JuceLibraryCode/AppConfig.h
@@ -0,0 +1,24 @@
+/*
+
+    IMPORTANT! This file is auto-generated each time you save your
+    project - if you alter its contents, your changes may be overwritten!
+
+    There's a section below where you can add your own custom code safely, and the
+    Projucer will preserve the contents of that block, but the best way to change
+    any of these definitions is by using the Projucer's project settings.
+
+    Any commented-out settings will assume their default values.
+
+*/
+
+#pragma once
+
+//==============================================================================
+// [BEGIN_USER_CODE_SECTION]
+
+// (You can add your own code in this section, and the Projucer will not overwrite it)
+#define JucePlugin_Build_LV2 1
+#define JucePlugin_LV2URI "https://socalabs.com/synths/virtualanalog"
+// [END_USER_CODE_SECTION]
+
+//==============================================================================
